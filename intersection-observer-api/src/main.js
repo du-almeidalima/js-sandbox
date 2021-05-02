df5a76b335de7a279dcab76312814d39bd7ab640
du-almeidalima <dualmeidalima@gmail.com>
2021-05-02 18:08:39 -0300

    Intersction Observer API Demo

---
+++ intersection-observer-api/src/main.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const sections = document.querySelectorAll('.section')
+
+/**
+ * @type IntersectionObserverInit
+ */
+const options = {
+  threshold: 0.5,
+  rootMargin: "0px"
+}
+
+const observer = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach(e => {
+          console.log(e)
+
+          if (e.isIntersecting) {
+            e.target.classList.add('section-intersected')
+          } else {
+            e.target.classList.remove('section-intersected')
+          }
+
+          // To stop observing a Element
+          // observer.unobserve(e.target)
+        })
+    },
+    options)
+
+sections.forEach(s => observer.observe(s))
+
